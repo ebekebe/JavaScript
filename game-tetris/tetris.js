326b0304394d12b167472b2adf41d52ba0270f3d
Digicrest <kaylaniwalker@gmail.com>
2018-01-06 20:29:21 +0000

    added colors

--- game-tetris/tetris.js
+++ game-tetris/tetris.js
@@ -3,10 +3,6 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-
-
-
-
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
     for (let y = 0; y < m.length; ++y) {
@@ -35,38 +31,38 @@ function createPiece(type) {
         ];
     } else if (type === 'O') {
         return  [
-            [1, 1],
-            [1, 1]
+            [2, 2],
+            [2, 2]
         ];
     } else if (type === 'L') {
         return  [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1]
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3]
         ];
     } else if (type === 'J') {
         return  [
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0]
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0]
         ];
     } else if (type === 'I') {
         return  [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0]
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0]
         ];
     } else if (type === 'S') {
         return  [
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0]
         ];
     } else if (type === 'Z') {
         return  [
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0]
         ];
     }
@@ -84,7 +80,7 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x, 
                     y + offset.y, 
                     1, 
@@ -183,6 +179,17 @@ function update(time = 0) {
     draw();
     requestAnimationFrame(update);
 }
+
+const colors = [
+    null,
+    '#FF0D72',
+    '#0DC2FF',
+    '#0DFF72',
+    '#F538FF',
+    '#FF8E0D',
+    '#FFE138',
+    '#3877FF'
+];
 
 const arena = createMatrix(12, 20);
 
