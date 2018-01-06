b89dd6353f9255ee24a15e72c2c807ec2a662271
Digicrest <kaylaniwalker@gmail.com>
2018-01-06 19:54:24 +0000

    created arena for saving state of locked pieces

--- game-tetris/tetris.js
+++ game-tetris/tetris.js
@@ -11,6 +11,13 @@ const matrix = [
     [0, 1, 0]
 ];
 
+function createMatrix(w, h) {
+    const matrix = [];
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
 
 function draw() {
     context.fillStyle = '#000';
@@ -34,6 +41,20 @@ function drawMatrix(matrix, offset) {
 
 }
 
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        });
+    });
+}
+
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0;
+}
 
 let dropCounter = 0;
 let dropInterval = 1000;
@@ -42,21 +63,40 @@ let lastTime = 0;
 function update(time = 0) {
     const deltaTime = time - lastTime;
     lastTime = time;
-    
+
     dropCounter += deltaTime;
     if(dropCounter > dropInterval) {
-        player.pos.y++;
-        dropCounter = 0;
+        playerDrop();
     }
 
     draw();
     requestAnimationFrame(update);
 }
 
+const arena = createMatrix(12, 20);
+
 const player = {
     pos: {x: 5, y: 5},
     matrix: matrix
 }
+
+document.addEventListener('keydown', event => {
+    switch(event.keyCode) {
+        case 37: 
+            player.pos.x--;
+            break;
+        case 39: 
+            player.pos.x++;
+            break;
+        case 40: 
+            playerDrop();
+            break;
+
+    }
+    if (event.keyCode === 37 ) {
+        player.pos.x--;
+    }
+});
 
 update();
 
