9448d8b5201e55c5232f3db41d3f7f8382bc766b
Digicrest <kaylaniwalker@gmail.com>
2018-01-12 23:52:15 +0000

    sky drawn

--- games/super-mario/public/js/main.js
+++ games/super-mario/public/js/main.js
@@ -13,5 +13,10 @@ loadImage('img/tiles.png').then(image => {
     const sprites = new SpriteSheet(image, 16, 16);
     sprites.define('ground', 0, 0);
     sprites.define('sky', 3, 23);
-    sprites.draw('sky', context, 45, 62);
+    
+    for(let x = 0; x < 25; x++) {
+        for(let y = 0; y < 14; ++y) {
+            sprites.draw('sky', context, x * 16, y * 16);
+        }
+    }
 });
